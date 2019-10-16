48aad46fb613c9046d25568f8f1141af3a61ecb4
yangziyang <superalvin123@foxmail.com>
2019-10-16 16:07:13 +0800

    change image bgr2rgb

--- detect.py
+++ detect.py
@@ -43,8 +43,8 @@ def main():
 
     save = torch.load(args.model_path)
     model.load_state_dict(save['model'])
-    img = cv2.imread(args.image_path)
-    # img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    src_img = cv2.imread(args.image_path)
+    img = cv2.cvtColor(src_img,cv2.COLOR_BGR2RGB)
     img_tensor = torch.from_numpy(img/255.).permute(2,0,1).float().cuda()
     input.append(img_tensor)
     out = model(input)
@@ -57,12 +57,15 @@ def main():
             x1, y1, x2, y2 = boxes[idx][0], boxes[idx][1], boxes[idx][2], boxes[idx][3]
             name = names.get(str(labels[idx].item()))
             # cv2.rectangle(img,(x1,y1),(x2,y2),colors[labels[idx].item()],thickness=2)
-            cv2.rectangle(img,(x1,y1),(x2,y2),random_color(),thickness=2)
-            cv2.putText(img, text=name, org=(x1, y1+10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
+            cv2.rectangle(src_img,(x1,y1),(x2,y2),random_color(),thickness=2)
+            cv2.putText(src_img, text=name, org=(x1, y1+10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
                 fontScale=0.5, thickness=1, lineType=cv2.LINE_AA, color=(0, 0, 255))
 
-    cv2.imshow('result',img)   
+    cv2.imshow('result',src_img)   
     cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    # cv2.imwrite('assets/11.jpg',img)
     
 
 if __name__ == "__main__":
